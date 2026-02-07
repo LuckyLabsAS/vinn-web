0665f1c61074b3eb8aaf82151cd05b0fa9347f6e
dagbrede-stb <dag.brede.ihle@storebrand.no>
2026-02-07 15:49:54 +0100

    Set up GitHub Pages Actions deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/vinnwin-web/",
+    base: "/",
     server: {
         port: 5173,
     },
